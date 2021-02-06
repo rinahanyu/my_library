c5ff92ae39080dedefb10264952ef295b1eb9b0c
EC2 Default User <ec2-user@ip-172-31-20-14.ec2.internal>
2021-02-06 13:52:05 +0000

    devise/view

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :address, :email,
+            presence: true
+  validates :telphone_number, :postal_code, numericality: { only_integer: true }
+  enum gender: {
+    男性: 0,
+    女性: 1,
+  }
+end
