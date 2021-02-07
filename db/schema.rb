2e7ab519be4272733f20752eee86908a5b847520
rinahanyu <gravity0112@gmail.com>
2021-02-07 14:00:18 +0000

    [Add]sign_in_path, genre_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210206141116) do
+ActiveRecord::Schema.define(version: 20210207135502) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name",                      null: false
+    t.boolean  "is_active",  default: true, null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "libraries", force: :cascade do |t|
     t.string   "name",            null: false
