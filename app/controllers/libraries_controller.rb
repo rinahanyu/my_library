8f5da23fb148b81044ff366fb40cdeff3f3c6e40
rinahanyu <gravity0112@gmail.com>
2021-02-08 14:50:46 +0000

    [Add]view/library_new

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,4 +1,17 @@
 class LibrariesController < ApplicationController
   def index
   end
+
+  def new
+    @library = Library.new
+  end
+  
+  def create
+    @library = Library.new(library_params)
+    if @library.save
+      redirect_to libraries_index_path
+    else
+      render "new"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :librarians
-  get 'libraries/index'
-
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
@@ -11,5 +8,7 @@ Rails.application.routes.draw do
     registrations: 'librarians/registrations',
     sessions: 'librarians/sessions'
   }
+
+  resources :libraries, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
