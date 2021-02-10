79b2b81001778104932b7da5996b066a9ee23174
rinahanyu <gravity0112@gmail.com>
2021-02-10 14:23:30 +0000

    [Add]controller/library_create,index

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,17 +1,24 @@
 class LibrariesController < ApplicationController
   def index
+    @libraries = Library.all
   end
 
   def new
     @library = Library.new
   end
-  
+
   def create
     @library = Library.new(library_params)
     if @library.save
-      redirect_to libraries_index_path
+      redirect_to libraries_path
     else
       render "new"
     end
+  end
+
+  private
+
+  def library_params
+    params.require(:library).permit(:name, :email, :postal_code, :address, :telphone_number)
   end
 end
