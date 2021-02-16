b588556977ad99f6d1f7cd63bd931d24a622321b
rinahanyu <gravity0112@gmail.com>
2021-02-16 12:57:01 +0000

    [Add]reservatioin_controller_create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,6 +4,24 @@ class ReservationsController < ApplicationController
     @book = Book.find(params[:book_id])
   end
 
+  def create
+    @book = Book.find(params[:book_id])
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user_id = current_user.id
+    @reservation.book_id = @book.id
+    if @reservation.save
+      redirect_to book_reservation_path(@reservation, @book)
+    else
+      render 'new'
+    end
+  end
+
   def show
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:lending_date, :return_date)
   end
 end
